8c057c5dc55017dcf80427f2fde0e68d1f01fdf5
gwest <gwest@gwest@redhat.com-mac>
2022-03-22 19:37:03 -0500

    Cleanup logging

--- I2CGateHa.ino
+++ I2CGateHa.ino
@@ -514,7 +514,7 @@ void onMqttConnect(bool sessionPresent) {
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-  Serial.println("Disconnected from MQTT.");
+  log("Disconnected from MQTT.");
 
   if (WiFi.isConnected()) {
     xTimerStart(mqttReconnectTimer, 0);
@@ -522,41 +522,41 @@ void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
 }
 
 void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
-  Serial.println("Subscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-  Serial.print("  qos: ");
-  Serial.println(qos);
+  //Serial.println("Subscribe acknowledged.");
+  //Serial.print("  packetId: ");
+  //Serial.println(packetId);
+  //Serial.print("  qos: ");
+  //Serial.println(qos);
 }
 
 void onMqttUnsubscribe(uint16_t packetId) {
-  Serial.println("Unsubscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
+  //Serial.println("Unsubscribe acknowledged.");
+  //Serial.print("  packetId: ");
+  //Serial.println(packetId);
 }
 
 void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
-  Serial.println("Publish received.");
-  Serial.print("  topic: ");
-  Serial.println(topic);
-  Serial.print("  qos: ");
-  Serial.println(properties.qos);
-  Serial.print("  dup: ");
-  Serial.println(properties.dup);
-  Serial.print("  retain: ");
-  Serial.println(properties.retain);
-  Serial.print("  len: ");
-  Serial.println(len);
-  Serial.print("  index: ");
-  Serial.println(index);
-  Serial.print("  total: ");
-  Serial.println(total);
+  //Serial.println("Publish received.");
+  //Serial.print("  topic: ");
+  //Serial.println(topic);
+  //Serial.print("  qos: ");
+  //Serial.println(properties.qos);
+  //Serial.print("  dup: ");
+  //Serial.println(properties.dup);
+  //Serial.print("  retain: ");
+  //Serial.println(properties.retain);
+  //Serial.print("  len: ");
+  //Serial.println(len);
+  //Serial.print("  index: ");
+  //Serial.println(index);
+  //Serial.print("  total: ");
+  //Serial.println(total);
 }
 
 void onMqttPublish(uint16_t packetId) {
-  Serial.println("Publish acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
+  //Serial.println("Publish acknowledged.");
+  //Serial.print("  packetId: ");
+  //Serial.println(packetId);
 }
 
 void epdlog(char *message)
