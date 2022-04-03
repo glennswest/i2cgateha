33b1b96bf7ee4225bcd4fb1300f61b7d50b10a48
gwest <gwest@gwest@redhat.com-mac>
2022-04-03 10:00:32 -0500

    First working testserver with dataupdate

--- testserver.js
+++ testserver.js
@@ -1,3 +1,8 @@
+const {
+  parse,
+  stringify,
+  assign
+} = require('comment-json');
 var fs = require('fs');
 var express = require('express');
 var app = express();
@@ -6,43 +11,38 @@ multer = require('multer');
 app.use(express.urlencoded({extended: false}));
 //handle static files
 app.use(express.static('contents')); //Serves resources from public folder
-app.use(express.static('data'));
+app.use('/data',express.static('data'));
 app.post('/data/:tname',multer().none(), function(req,res,next){
-         data=JSON.stringify(req.body);
-         save(data,req."data/"+params.tname);
-         res.sendFIle(params.tname);
+         console.log(req.body);
+         data=stringify(req.body);
+         data=parse(data);
+         save(data,"./data/" + req.params.tname);
+         res.sendFile(__dirname + "/data/" + req.params.tname);
          });
 
 var server = app.listen(8080);
 
 
 function save(item, path = './collection.json'){
-    if (!fs.existsSync(path)) {
-        fs.writeFile(path, JSON.stringify([item]));
-    } else {
-        var data = fs.readFileSync(path, 'utf8');  
-        var list = (data.length) ? JSON.parse(data): [];
-        if (list instanceof Array) list.push(item)
-        else list = [item]  
-        fs.writeFileSync(path, JSON.stringify(list));
-    }
-}
-
-function save(item, path = './collection.json'){
     console.log("save: " + path + " - " + item);
     if (!fs.existsSync(path)) {
-        fs.writeFile(path, "[" + JSON.stringify([item]) + "]");
+        fs.writeFile(path, "[" + item + "]");
     } else {
         var data = fs.readFileSync(path, 'utf8');  
-        var list = JSON.parse(data);
+        console.log(data);
+        var list = parse(data);
         for(var i = 0; i < list.length; i++){
-          if (item.id = list[i].id){
+          console.log(list[i]);
+          console.log(item.id);
+          if (item.id == list[i].id){
             console.log("Updating item");
             list[i] = item;
             updated = true;
             }
           }
-       fs.writeFileSync(path, JSON.stringify(list));
+       result =stringify(list,null,2);
+       console.log(result);
+       fs.writeFileSync(path, result);
        } 
 }
 
